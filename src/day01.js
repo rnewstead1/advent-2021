75b0e852b6f9ae2c83df7f8ee4ebb1ff939799e5
Rachel Normand <racheljnewstead@gmail.com>
2021-12-01 21:28:24 +0000

    Day 1 part 2

--- src/day01.js
+++ src/day01.js
@@ -4,18 +4,34 @@ const getLines = (filename) =>
   readFile(filename, { encoding: 'utf8' })
     .then((contents) => contents.toString().trim().split('\n'));
 
-const getNumberOfLargerMeasurements = async (dataFile) => {
-  const depths = await getLines(dataFile)
-  const depthsOffsetByOne = depths.slice(1, depths.length)
+const numericArray = (length) => [...Array(length).keys()];
 
-  const areMeasurementsLarger = [...Array(depthsOffsetByOne.length).keys()]
-    .map((i) => {
-      return parseInt(depths[i]) < parseInt(depthsOffsetByOne[i])
-    });
+const increaseCount = (list) => {
+  const listOffsetByOne = list.slice(1, list.length)
 
-  return areMeasurementsLarger.filter(Boolean).length;
+  return numericArray(listOffsetByOne.length)
+    .map(i => list[i] < listOffsetByOne[i])
+    .filter(Boolean).length;
 }
 
-module.exports = {
-  getNumberOfLargerMeasurements,
+module.exports = async (datafile) => {
+  const lines = await getLines(datafile);
+  const depths = lines.map(line => parseInt(line));
+
+  const singleIncreaseCount = () => increaseCount(depths);
+
+  const trebleIncreaseCount = () => {
+    const listFromStart = depths.slice(0, depths.length - 2);
+    const listFromNext = depths.slice(1, depths.length - 1);
+    const listFromNextNext = depths.slice(2, depths.length);
+    const differences = numericArray(listFromStart.length)
+      .map(i => listFromStart[i] + listFromNext[i] + listFromNextNext[i])
+
+    return increaseCount(differences);
+  }
+
+  return {
+    singleIncreaseCount,
+    trebleIncreaseCount,
+  }
 }
--- test/day01.test.js
+++ test/day01.test.js
@@ -1,13 +1,21 @@
 const { expect } = require('chai');
 
-const { getNumberOfLargerMeasurements } = require('../src/day01')
+const initDay1 = require('../src/day01')
 
 const DATA_FILE = __dirname + '/data/day01.txt';
 
 describe('day 1', () => {
-  it('should return 7 larger measurements', async () => {
-    const numberOfLargerMeasurements = await getNumberOfLargerMeasurements(DATA_FILE);
+  let day1;
 
-    expect(numberOfLargerMeasurements).to.equal(7);
+  before(async () => {
+    day1 = await initDay1(DATA_FILE);
+  });
+
+  it('should return 7 single increases', async () => {
+    expect(day1.singleIncreaseCount()).to.equal(7);
   })
+
+  it('should return 5 treble increases', function () {
+    expect(day1.trebleIncreaseCount()).to.equal(5);
+  });
 })
