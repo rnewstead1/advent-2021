9425b9c2013b9d96a54380044bacabfb26a8cfd1
Rachel Normand <racheljnewstead@gmail.com>
2021-12-03 21:26:42 +0000

    refactor so part 2 uses part 1

--- src/day03.js
+++ src/day03.js
@@ -4,16 +4,19 @@ const numericArray = (length) => [...Array(length).keys()];
 
 const asDecimal = (gammaRate) => parseInt(gammaRate.join(''), 2);
 
-function filterMatrix(originalMatrix, bitToKeepFn) {
+const getBitToKeep = (matrix, index, bitToKeepFn) => {
+  let oneCount = 0;
+  matrix.forEach(row => {
+    oneCount += row[index];
+  })
+  return bitToKeepFn(oneCount, matrix.length) ? 1 : 0
+};
+
+const filterMatrix = (originalMatrix, bitToKeepFn) => {
   let matrix = originalMatrix;
   let i = 0;
   while (matrix.length > 1) {
-    let oneCount = 0;
-    let bitToKeep;
-    matrix.forEach(row => {
-      oneCount += row[i];
-    });
-    bitToKeep = bitToKeepFn(oneCount, matrix.length) ? 1 : 0;
+    const bitToKeep = getBitToKeep(matrix, i, bitToKeepFn);
     matrix = matrix.filter(row => row[i] === bitToKeep)
     i++;
   }
@@ -22,17 +25,14 @@ function filterMatrix(originalMatrix, bitToKeepFn) {
 
 module.exports = async (datafile) => {
   const lines = await getLines(datafile);
-  const columns = numericArray(lines[0].length)
-    .map(i => lines
-      .map(line => parseInt(line[i])));
-
+  const indexes = numericArray(lines[0].length);
+  indexes.map(i => lines.map(line => parseInt(line[i])));
   const matrix = lines.map(line => line.split('').map(bit => parseInt(bit)));
 
   return {
     powerConsumption: () => {
-      const countOfOnesInColumn = columns.map(column => column.reduce((acc, curr) => acc + curr, 0));
-      const gammaRate = countOfOnesInColumn.map(count => count > lines.length / 2 ? 1 : 0);
-      const epsilonRate = countOfOnesInColumn.map(count => count < lines.length / 2 ? 1 : 0);
+      const gammaRate = indexes.map(i => getBitToKeep(matrix, i, (oneCount, length) => oneCount > length / 2));
+      const epsilonRate = indexes.map(i => getBitToKeep(matrix, i, (oneCount, length) => oneCount < length / 2));
 
       return asDecimal(gammaRate) * asDecimal(epsilonRate);
     },
@@ -40,6 +40,7 @@ module.exports = async (datafile) => {
     lifeSupportRating: () => {
       const oxygenGeneratorRating = filterMatrix(matrix, (oneCount, length) => oneCount >= length / 2);
       const co2ScrubberRating = filterMatrix(matrix, (oneCount, length) => oneCount < length / 2);
+
       return asDecimal(oxygenGeneratorRating) * asDecimal(co2ScrubberRating);
     },
   }
