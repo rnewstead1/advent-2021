3038b4da076e0c3737a92643e2cbea4c5af318aa
Rachel Normand <racheljnewstead@gmail.com>
2021-12-06 23:04:14 +0000

    day 6 part 2
    lots of OOM fun!

--- src/day06.js
+++ src/day06.js
@@ -1,4 +1,4 @@
-const { getLines } = require('./helpers');
+const { getLines, numericArray } = require('./helpers');
 
 module.exports = async (datafile) => {
   const lines = await getLines(datafile);
@@ -6,7 +6,7 @@ module.exports = async (datafile) => {
 
   return {
     fishCount: (days) => {
-      const countFish = (fish, day) => {
+      const countFish = (fish, day=0) => {
         if (day === days) {
           return fish;
         }
@@ -20,7 +20,33 @@ module.exports = async (datafile) => {
         });
         return countFish(newFish, day + 1);
       }
-      return countFish([...originalFish], 0).length;
+
+      return countFish([...originalFish]).length;
+    },
+    fishCountPart2: (days) => {
+      const fishCount = {}
+      numericArray(9).forEach(i => {
+        fishCount[i] = 0;
+      })
+      originalFish.forEach(x => {
+        fishCount[x]++;
+      })
+
+      let day = 1
+      while (day <= days) {
+        const fishReadyToCreate = fishCount[0];
+        fishCount[0] = fishCount[1];
+        fishCount[1] = fishCount[2];
+        fishCount[2] = fishCount[3];
+        fishCount[3] = fishCount[4];
+        fishCount[4] = fishCount[5];
+        fishCount[5] = fishCount[6];
+        fishCount[6] = fishCount[7] + fishReadyToCreate;
+        fishCount[7] = fishCount[8];
+        fishCount[8] = fishReadyToCreate;
+        day++;
+      }
+      return Object.values(fishCount).reduce((acc, curr) => acc + curr, 0);
     },
   }
 }
--- test/day06.test.js
+++ test/day06.test.js
@@ -13,9 +13,15 @@ describe('day 6', () => {
 
   it('should return a total of 26 fish after 18 days', () => {
     expect(day6.fishCount(18)).to.equal(26);
+    expect(day6.fishCountPart2(18)).to.equal(26);
   });
 
   it('should return return a total of 5934 fish after 80 days', () => {
     expect(day6.fishCount(80)).to.equal(5934);
+    expect(day6.fishCountPart2(80)).to.equal(5934);
+  });
+
+  it('should return return a total of 26984457539 fish after 256 days', () => {
+    expect(day6.fishCountPart2(256)).to.equal(26984457539);
   });
 })
