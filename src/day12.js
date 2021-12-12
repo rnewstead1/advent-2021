10fe7634e13f2af387bc691864a981d7ac335e5e
Rachel Normand <racheljnewstead@gmail.com>
2021-12-12 18:17:30 +0000

    day 12 part 2

--- src/day12.js
+++ src/day12.js
@@ -15,24 +15,40 @@ const buildCaveMap = (lines) => {
   return caveMap;
 };
 
-const findRoute = (caveMap, routeSoFar) => {
+const cannotRevisitSmallCaves = (cave, routeSoFar) => cave.toUpperCase() === cave || !routeSoFar.includes(cave);
+
+const hasNotAlreadyVisitedASmallCave = (cave, routeSoFar) => {
+  const smallCavesVisited = routeSoFar.filter(cave => cave.toLowerCase() === cave);
+  return smallCavesVisited.length === new Set(smallCavesVisited).size;
+}
+
+const isAllowedToRevisitASmallCave = (cave, routeSoFar) => cave !== START
+  ? hasNotAlreadyVisitedASmallCave(cave, routeSoFar) : false;
+
+const canRevisitASmallCave = (cave, routeSoFar) => cannotRevisitSmallCaves(cave, routeSoFar)
+  || isAllowedToRevisitASmallCave(cave, routeSoFar);
+
+const findRoute = (caveMap, routeSoFar, followsRules) => {
   const cave = routeSoFar[routeSoFar.length - 1];
   if (cave === DESTINATION) {
     return routeSoFar.join();
   }
   return caveMap[cave]
-    .filter(cave => cave.toUpperCase() === cave || !routeSoFar.includes(cave))
-    .flatMap(cave => findRoute(caveMap, [...routeSoFar, cave]));
+    .filter(cave => followsRules(cave, routeSoFar))
+    .flatMap(cave => findRoute(caveMap, [...routeSoFar, cave], followsRules));
 };
 
 module.exports = async (datafile) => {
   const lines = await getLines(datafile);
+  const caveMap = buildCaveMap(lines);
 
   return {
-    distinctPaths: () => {
-      const caveMap = buildCaveMap(lines);
-      const routes = findRoute(caveMap, [START]);
-
+    distinctPathsWithoutRevisitingSmallCaves: () => {
+      const routes = findRoute(caveMap, [START], cannotRevisitSmallCaves);
+      return routes.length;
+    },
+    distinctPathsThatCanRevisitOneSmallCave: () => {
+      const routes = findRoute(caveMap, [START], canRevisitASmallCave);
       return routes.length;
     },
   }
--- test/day12.test.js
+++ test/day12.test.js
@@ -3,15 +3,38 @@ const { expect } = require('chai');
 const initDay12 = require('../src/day12')
 
 const DATA_FILE = __dirname + '/data/day12.txt';
+const MEDIUM_DATA_FILE = __dirname + '/data/day12-medium.txt';
 
 describe('day 12', () => {
-  let day12;
+  describe('small data file', () => {
+    let day12;
 
-  before(async () => {
-    day12 = await initDay12(DATA_FILE);
+    before(async () => {
+      day12 = await initDay12(DATA_FILE);
+    });
+
+    it('should return 10 distinct paths without revisiting small caves', () => {
+      expect(day12.distinctPathsWithoutRevisitingSmallCaves()).to.equal(10);
+    });
+
+    it('should return 36 distinct paths that can revisit one small cave', () => {
+      expect(day12.distinctPathsThatCanRevisitOneSmallCave()).to.equal(36);
+    });
   });
 
-  it('should return 10 distinct paths', () => {
-    expect(day12.distinctPaths()).to.equal(10);
-  });
+  describe('medium data file', () => {
+    let day12;
+
+    before(async () => {
+      day12 = await initDay12(MEDIUM_DATA_FILE);
+    });
+
+    it('should return 19 distinct paths without revisiting small caves', () => {
+      expect(day12.distinctPathsWithoutRevisitingSmallCaves()).to.equal(19);
+    });
+
+    it('should return 103 distinct paths that can revisit one small cave', () => {
+      expect(day12.distinctPathsThatCanRevisitOneSmallCave()).to.equal(103);
+    });
+  })
 })
